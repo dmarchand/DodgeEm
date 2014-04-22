fb542eb38045dc7d67ea734ea17c3c858d86f274
Dan Marchand <dan.marchand@gmail.com>
2014-04-22 00:25:41 -0400

    Turrets and shit

---
+++ DodgeEmFinal/Assets/ProjectileController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+	public int Damage;
+	public string Name;
+	public bool DestroyOnDeath;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if(DestroyOnDeath)
+		{
+			Destroy(this.gameObject);
+		}
+
+		Debug.Log("Collision");
+	}
+}
